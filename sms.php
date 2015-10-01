9a6c9394f0a0b09d436ae24b39d7f91ffe604ad7
oper29a@gmail.com <oper29a@gmail.com>
2015-10-01 16:55:05 +0500

    no message

---
+++ sms.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Everus\SMScGate\Message;
+
+class message
+{
+	// текст сообщения
+	private $text;
+	// список телефонов
+	private $phones = array();
+	// формат сообщения (0 - обычное sms, 1 - flash-sms, 2 - wap-push, 3 - hlr, 4 - bin, 5 - bin-hex, 6 - ping-sms, 7 - mms, 8 - mail, 9 - call)
+	private $formats = array(
+		1 => "flash=1", 
+		"push=1", 
+		"hlr=1", 
+		"bin=1", 
+		"bin=2", 
+		"ping=1", 
+		"mms=1", 
+		"mail=1", 
+		"call=1");
+	private $format;
+	// $time - необходимое время доставки в виде строки (DDMMYYhhmm, h1-h2, 0ts, +m)
+	private $time;
+	private $query;
+
+	public function getPhones()
+	{
+		return $this->phones;
+	}
+
+	public function setPhones($phones)
+	{
+		$this->phones=$phones;
+	}
+
+	public function addPhone($phone)
+	{
+		$this->phones[] = $phone;
+	}
+
+	public function deletePhone($phone)
+	{
+		foreach ($this->phones as $key => $value) {
+			if($value === $phone) {
+				unset($this->phones[$key]);
+				break;
+			}
+		}
+	}
+}
--- smsgate.php
+++ smsgate.php
@@ -1,2 +1,50 @@
 <?php
 
+namespace Everus\SMScGate\Transport;
+
+class httpTransport 
+{
+	const DEFAUL_CHARSET = 'windows-1251';
+
+	private $login;
+	private $password;
+	// кодировка
+	private $charset;
+
+	public function __construct($login, $password, $charset = DEFAUL_CHARSET) 
+	{
+		$this->login = $login;
+		$this->password = $password;
+		$this->charset = $charset;
+	}
+
+	public function getLogin()
+	{
+		return $this->login;
+	}
+
+	public function setLogin($login)
+	{
+		$this->login = $login;
+	}
+
+	public function getPassword()
+	{
+		return $this->password;
+	}
+
+	public function setPassword($password)
+	{
+		$this->password = $password;
+	}
+
+	public function getCharset()
+	{
+		return $this->charset;
+	}
+
+	public function setCharset($charset)
+	{
+		$this->charset = $charset;
+	}
+}
